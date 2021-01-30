8fc05a7ed453b1b9fbe713e40197bb4e6c7e1e24
Yeshasvitvs <yeshasvitvs@gmail.com>
2021-01-30 13:13:10 +0100

    Address todo from server constructor

--- tcp_test_application/tcp_server.cpp
+++ tcp_test_application/tcp_server.cpp
@@ -7,31 +7,61 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+// Reference : https://pubs.opengroup.org/onlinepubs/9699919799/
+// search for socket.h
+
+using namespace std;
+
 //---------------------------------------------------------------------------------------------------------------------
 tcp_server::tcp_server(int port) {
 
-    std::cout << "[info] server init" << std::endl;
+    std::cout << "[info] server initialization in progress ... " << std::endl;
 
     socket_handle = socket(AF_INET, SOCK_STREAM, 0);
 
+    // Check status of server socket fd
+    if (socket_handle == 0)
+    {
+        std::cerr << "[error] server socket initialization failed!" << std::endl;
+        return;
+    }
+
+    // Socket fd configuration options buffer
     std::vector<int> options = {SO_REUSEPORT, SO_REUSEADDR};
 
-    // todo: check for errors, report them
-    setsockopt(socket_handle, SOL_SOCKET, 2, (char*)&options, sizeof(options));
+    // setsockopt(socket_handle, SOL_SOCKET, 2, (char*)&options, sizeof(options));
+    // Configure socket fd with option SO_REUSEADDR (2)
+    if( setsockopt(socket_handle, SOL_SOCKET, options[1], (char*)&options, sizeof(options)) == -1)
+    {
+        std::cerr << "[error] server socket options configuration failed!" << std::endl;
+        return;
+    }
 
+    // Set socket server address
     memset(&server_address, 0, sizeof(server_address));
 
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = htonl(INADDR_ANY);
     server_address.sin_port = htons(port);
 
-    // todo: check for errors
-    bind(socket_handle, (struct sockaddr *)&server_address, sizeof(server_address));
+    // check for errors
+    if( bind(socket_handle, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
+    {
+        std::cerr << "[error] server socket binding failed to port " << port << std::endl;
+        return;
+    }
 
-    // todo: check for errors
-    listen(socket_handle, 1);
+    // Check for errors
+    // TODO: Change backlog option to have multiple client connections
+    if(listen(socket_handle, 1) == -1)
+    {
+        std::cerr << "[error] server socket at port " << port << " failed to start listening!" << std::endl;
+        return;
+    }
 
     // todo: handle errors of this constructor
+    std::cout << "[info] server initialization done successfully ... " << std::endl;
+    std::cout << "[info] server socket port : " << port << std::endl;
 }
 
 //---------------------------------------------------------------------------------------------------------------------
